d264f411acf9b2f57b0198235bcf45bd60eb9205
Shubhdeep Verma <shubhdeepverma@Shubhdeeps-MacBook-Air.local>
2024-07-08 18:30:16 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+// console.log(name);
+
+// var name = "Shubh";
+
+// display();
+
+// function display() {
+//   console.log("in Display");
+// }
+
+// display();
+// var display = () => {
+//   console.log("In display");
+// };
+
+console.log(name);
+let name = "Shubhdeep";
