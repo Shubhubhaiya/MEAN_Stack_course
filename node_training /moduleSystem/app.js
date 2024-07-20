db05a82897fd30d776f95aa1c281dea898f06770
Shubhdeep Verma <shubhdeepverma@Shubhdeeps-MacBook-Air.local>
2024-07-20 20:28:31 +0530

    module in node js

---
+++ node_training /moduleSystem/app.js
@@ -0,0 +1,10 @@
+// const { sum } = require("./mathOperation");
+const { sub } = require("./mathOperation");
+const { multiply } = require("./mathOperation");
+const { sum } = require("./mathOperation");
+const { name } = require("./mathOperation");
+
+console.log(name);
+console.log(sub(4, 5));
+console.log(sum(4, 5));
+console.log(multiply(4, 5));
---
+++ node_training /moduleSystem/mathOperation.js
@@ -0,0 +1,15 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num2 - num1;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+var name = "shubhdeep";
+
+module.exports = { sum, sub, multiply, name };
